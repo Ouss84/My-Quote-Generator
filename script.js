50e8740107f26adbeb5137fe64df26746e2800dc
Ouss Bahri <bahrioussama84@gmail.com>
2020-12-22 23:35:28 +0200

    adding the getQuote function from API

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get Quote function from API
+async function getQuote () {
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+try{
+const res = await fetch (proxyUrl + apiUrl);
+const data = await res.json();
+console.log(data);
+} catch(error){
+    getQuote();
+    console.log('No Quote to display!', error);
+}
+}
+
+//On Load
+getQuote();

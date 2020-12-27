d9b3bf0baa6600cc060416aa99f4f072bf76b0a8
Ouss Bahri <bahrioussama84@gmail.com>
2020-12-27 19:57:21 +0200

    adding new proxy with heroku

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get Quote function from API
 async function getQuote () {
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const proxyUrl = 'https://pacific-escarpment-24387.herokuapp.com'
 const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 try{
 const res = await fetch (proxyUrl + apiUrl);
